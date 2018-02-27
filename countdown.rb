ad1030c5b9a731118c7a04939a459b8fb1d863b4
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-02-27 19:12:28 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,6 +7,11 @@ def countdown(seconds)
   end
   "HAPPY NEW YEAR!"
 end
-def countdown_with_sleep(seconds)
 
+def countdown_with_sleep(seconds)
+  seconds.times do
+    "#{seconds} SECOND(S)!"
+    seconds -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
